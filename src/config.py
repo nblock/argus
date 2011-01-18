5b7d9baffb638f4756b6334219a378507608f7ab
Florian Preinstorfer <nblock@archlinux.us>
2011-01-18 21:39:55 +0100

    added config

---
+++ src/config.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+##
+# project: this file is part of argus
+# author: notizblock <nblock@archlinux.us>
+# license: GPLv3
+
+class Config():
+    '''configuration parameters for argus'''
+    #TODO: 
+    # -add support for config file
+    # - validation
+    # - commandline parameters
+
+    #max url's per pastebin
+    max_urls_per_pastebin = 10
+
+    #how many threads should be used for fetching
+    fetch_threads = 2
+
+    #how many threads should be used for datamining
+    datamine_threads = 1
+    
+    #extension directory
+    extensions_directory = 'extensions/'
+
+    #http timeout in seconds (global)
+    http_timeout = 5
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 smartindent autoindent 
